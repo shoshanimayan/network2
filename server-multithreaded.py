b5d3f442639ba2ee94c7be9831368b84ae218f66
PGrad <pgrad@uci.edu>
2017-10-21 14:01:21 -0700

    Added .gitignore, basic multithreaded server

--- server-multithreaded.py
+++ server-multithreaded.py
@@ -1,32 +1,45 @@
 #import socket module
 from socket import *
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-#Fill in start
-#Fill in end
-while True:
-# Establish the connection
-    print ("Ready to serve...")
-    connectionSocket, addr = #Fill in start Fill in end
-    try:
-        message = #Fill in start Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:], "rb")
-        outputdata = #Fill in start Fill in end
-#Send one HTTP header line into socket
-#Fill in start
-#Fill in end
-#Send the content of the requested file to the client
-    for i in range(0,len(outputdata)):
-        connectionSocket.send(outputdata[i:i+1])
-    connectionSocket.send(b'\r\n\r\n')
-    connectionSocket.close()
-    except IOError:
+import thread
 
-#Send response message for file not found
-#Fill in Start
-#Fill in end
-#Close client socket
-#Fill in start
-#Fill in end
+def worker(conn, addr):
+    try:
+        message = conn.recv(2048)
+        filename = message.split()[1]
+        with open(filename[1:], "rb") as f:
+            outputdata = f.read()
+            #Send one HTTP header line into socket
+            conn.send("HTTP/1.1 200 OK\n")
+            #Send the content of the requested file to the client
+            for i in range(0,len(outputdata)):
+                connectionSocket.send(outputdata[i:i+1])
+            conn.send(b'\r\n\r\n')
+            conn.close()
+    except IOError:
+        #Send response message for file not found
+        #Fill in Start
+        conn.send("HTTP/1.1 404 Not Found\n")
+        #Fill in end
+        #Close client socket
+        #Fill in start
+        conn.close()
+        #Fill in end
+
+def main():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a sever socket
+    #Fill in start
+    serverPort = 12000
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(1)
+    #Fill in end
+    while True:
+    # Establish the connection
+        print ("Ready to serve...")
+        connectionSocket, addr = serverSocket.accept()
+        thread.start_new_thread(worker, (connectionSocket, addr))
+
     serverSocket.close()
+
+if __name__=="__main__":
+    main()
