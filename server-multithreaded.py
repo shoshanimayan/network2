bf45b51bf9940ea5ca63cb2c8a6c63d32f428b46
PGrad <paulgrad3rd@gmail.com>
2017-10-21 16:34:15 -0700

    Added lab2_files and a .giignore. My server is ignoring images for now

--- server-multithreaded.py
+++ server-multithreaded.py
@@ -1,6 +1,6 @@
 #import socket module
 from socket import *
-import thread
+import _thread
 
 def worker(conn, addr):
     try:
@@ -8,17 +8,18 @@ def worker(conn, addr):
         filename = message.split()[1]
         with open(filename[1:], "rb") as f:
             outputdata = f.read()
+            content_length = len(outputdata) // 8
             #Send one HTTP header line into socket
-            conn.send("HTTP/1.1 200 OK\n")
+            conn.send(b'HTTP/1.1 200 OK\n')
             #Send the content of the requested file to the client
             for i in range(0,len(outputdata)):
-                connectionSocket.send(outputdata[i:i+1])
+                conn.send(outputdata[i:i+1])
             conn.send(b'\r\n\r\n')
             conn.close()
     except IOError:
         #Send response message for file not found
         #Fill in Start
-        conn.send("HTTP/1.1 404 Not Found\n")
+        conn.send(b'HTTP/1.1 404 Not Found\n')
         #Fill in end
         #Close client socket
         #Fill in start
@@ -37,7 +38,7 @@ def main():
     # Establish the connection
         print ("Ready to serve...")
         connectionSocket, addr = serverSocket.accept()
-        thread.start_new_thread(worker, (connectionSocket, addr))
+        _thread.start_new_thread(worker, (connectionSocket, addr))
 
     serverSocket.close()
 
