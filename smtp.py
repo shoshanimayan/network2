7de278fb035ecebb4d39377e7d983a0e2b63a0c0
Mayan Shoshani <mshoshan@uci.edu>
2017-10-21 17:09:53 -0700

    copy paste

--- smtp.py
+++ smtp.py
@@ -5,6 +5,10 @@ endmsg = "\r\n.\r\n"
 mailserver = "localhost"
 # Create socket called clientSocket and establish a TCP connection with mailserver
 #Fill in start
+clientSocket = socket(AF_INET, SOCK_STREAM)]
+serverPort =25
+clientSocket.connect((mailserver,serverPort))
+
 #Fill in end
 recv = clientSocket.recv(1024).decode()
 print(recv)
@@ -19,19 +23,55 @@ if recv1[:3] != '250':
     print('250 reply not received from server.')
 # Send MAIL FROM command and print server response.
 # Fill in start
+heloCommand = 'MAIL FROM mshoshan@uci.edu\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
 # Send RCPT TO command and print server response.
 # Fill in start
+heloCommand = 'RCPT TO \r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
 # Send DATA command and print server response.
 # Fill in start
+heloCommand = 'DATA\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
 # Send message data.
 # Fill in start
+heloCommand = 'hi\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
 # Message ends with a single period.
 # Fill in start
+heloCommand = '.\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
 # Send QUIT command and get server response.
 # Fill in start
+heloCommand = 'QUIT\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
 # Fill in end
