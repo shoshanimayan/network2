1883110cf7d0ab6fec65250b09b8a06acfb6d9b8
PGrad <paulgrad3rd@gmail.com>
2017-10-23 16:43:54 -0700

    Can now fetch url

--- httpClient.py
+++ httpClient.py
@@ -1,7 +1,10 @@
 import http.client
+from sys import argv
+if(len(argv) != 2):
+    print("Usage: <httpclient.py> <url>")
+    exit(1)
 conn = http.client.HTTPConnection('localhost',12000)
-conn.set_tunnel("http://169.234.54.57:6789/lab2.html")
-conn.request("GET", "/")
+conn.request("GET", "/" + argv[1])
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 data1 = r1.read()
