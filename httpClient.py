d914fb1dbc137f1709082c40fe3669c293908365
PGrad <paulgrad3rd@gmail.com>
2017-10-23 18:13:22 -0700

    Modifying args to fit the hw assignment

--- httpClient.py
+++ httpClient.py
@@ -1,14 +1,14 @@
 import http.client
 from sys import argv
-if(len(argv) != 3):
+if(len(argv) != 4):
     print(argv)
-    print("Usage: httpclient.py <ip address> <port> <requested page>")
+    print("Usage: httpclient.py <server_host> <server_port> <filename>")
     exit(1)
 url = argv[1]+":"+argv[2]
 print (url)
 conn = http.client.HTTPConnection(argv[1], int(argv[2]))
 #lab1
-conn.request("GET", "/lab1.html")
+conn.request("GET", "/" + argv[3])
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 data1 = r1.read()
