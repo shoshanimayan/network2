366047ab70cb043c5a98ab70424be74015934ca3
Mayan Shoshani <mshoshan@uci.edu>
2017-10-23 17:34:26 -0700

    new

--- httpClient.py
+++ httpClient.py
@@ -1,12 +1,14 @@
 import http.client
 from sys import argv
-if(len(argv) != 2):
-    print("Usage: <httpclient.py> <url>")
+if(len(argv) != 3):
+    print("Usage: <httpclient.py> <ip address> <port>")
     exit(1)
-conn = http.client.HTTPConnection('localhost',12000)
-conn.request("GET", "/" + argv[1])
+
+conn = http.client.HTTPConnection('localhost', 12000)
+url = argv[1]+":"+argv[2]+"/lab1.html"
+print(url)
+conn.request("GET", "/" + argv[1]+":"+argv[2]+"/lab1.html")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 data1 = r1.read()
 print(data1)
-
