65361335abe273823e209a9bf747650cbbf27544
Mayan Shoshani <mshoshan@uci.edu>
2017-10-23 17:59:14 -0700

    works now, prints both lab1 and lab2 data

--- httpClient.py
+++ httpClient.py
@@ -7,9 +7,15 @@ if(len(argv) != 3):
 url = argv[1]+":"+argv[2]
 print (url)
 conn = http.client.HTTPConnection(argv[1], int(argv[2]))
+#lab1
 conn.request("GET", "/lab1.html")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 data1 = r1.read()
+#lab2
+conn.request("GET", "/lab2.html")
+r2 = conn.getresponse()
+print(r2.status, r2.reason)
+data2 = r2.read()
 print(data1)
-
+print(data2)
