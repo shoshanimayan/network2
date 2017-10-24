2cc650d3cf244b924a6e6f3073a540adcc2d6e3f
PGrad <paulgrad3rd@gmail.com>
2017-10-23 18:25:06 -0700

    Following hw assignment specs, getting response of (host,port,path) specified on command line.

--- httpClient.py
+++ httpClient.py
@@ -4,18 +4,9 @@ if(len(argv) != 4):
     print(argv)
     print("Usage: httpclient.py <server_host> <server_port> <filename>")
     exit(1)
-url = argv[1]+":"+argv[2]
-print (url)
 conn = http.client.HTTPConnection(argv[1], int(argv[2]))
-#lab1
 conn.request("GET", "/" + argv[3])
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
-data1 = r1.read()
-#lab2
-conn.request("GET", "/lab2.html")
-r2 = conn.getresponse()
-print(r2.status, r2.reason)
-data2 = r2.read()
-print(data1)
-print(data2)
+data = r1.read()
+print(data)
